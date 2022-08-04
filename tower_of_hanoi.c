8518d9e67ee03995386f57fbc23d5351442bc3b9
Rohit Luni <87267331+rohit4242@users.noreply.github.com>
2022-08-04 15:26:39 +0530

    Update tower_of_hanoi.c

--- tower_of_hanoi.c
+++ tower_of_hanoi.c
@@ -3,5 +3,18 @@
 void transfer(int n,char A,char B,char C)
 {
     if(n==1)
+    {
+        printf("\n Move disk 1 from %c to %c", A, C);
+        return 0;
+    }
+    transfer(n-1, A, C, B);
+    printf("\n Move disk %d from rod %c to rod %c", n, A, C);
+    transfer(n-1, B, C, A);
+}
         
+int main()
+{
+    int n = 4;
+    transfer(n, 'A', 'B', 'C'); 
+    return 0;
 }
